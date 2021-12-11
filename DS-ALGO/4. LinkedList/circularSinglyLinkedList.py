5016fd49a21db7761af3f56e5228adcb29e6f37f
Arnab Dutta <arnab.dutta558.ad@gmail.com>
2021-12-11 19:23:03 +0530

    Adding Circular LinkedList Codes

--- DS-ALGO/4. LinkedList/circularSinglyLinkedList.py
+++ DS-ALGO/4. LinkedList/circularSinglyLinkedList.py
@@ -17,12 +17,68 @@ class CircularSLL:
     
     def createCSLL(self,nodeValue):
         node = Node(nodeValue)
-        node.next = node
         self.head = node
         self.tail = node
+        node.next = node
         return "CSLL has been created"
+
+    def insertCSLL(self,nodeValue,position):
+        if self.head is None:
+            return "Circular Singly Linked List Does Not Exist"
+        else:
+            newNode = Node(nodeValue)
+            if position == 0:
+                newNode.next = self.head
+                self.head = newNode
+                self.tail.next = newNode
+            elif position == 1:
+                newNode.next = self.tail.next
+                self.tail.next = newNode
+                self.tail = newNode
+            else:
+                index = 0
+                tempNode = self.head
+                while index < position - 1:
+                    tempNode = tempNode.next
+                    index += 1
+                nextNode = tempNode.next
+                tempNode.next = newNode
+                newNode.next = nextNode
+            return "Node Has been successfully created"
+    def traverseCSLL(self):
+        if self.head is None:
+            return "Circular Singly Linked List Does Not Exist"
+        
+        else:
+            tempNode = self.head
+            while tempNode:
+                print(tempNode.value)
+                tempNode = tempNode.next
+                if tempNode == self.tail.next:
+                    break
+
+    def searchCSLL(self,value):
+        if self.head is None:
+            return "Circular Singly Linked List Does Not Exist"
+        else:
+            tempNode = self.head
+            while tempNode:
+                if tempNode.value == value:
+                    return tempNode.value
+                tempNode = tempNode.next
+                if tempNode == self.tail.next:
+                    return "Value Does not Exist in Circular SLL"
+
+            
 
 circularSLL = CircularSLL()
 circularSLL.createCSLL(1)
-
+circularSLL.insertCSLL(2,0)
+circularSLL.insertCSLL(3,1)
+circularSLL.insertCSLL(4,1)
+circularSLL.insertCSLL(5,1)
+circularSLL.insertCSLL(22,4)
 print([node.value for node in circularSLL])
+circularSLL.traverseCSLL()
+print(circularSLL.searchCSLL(200))
+
