ac0876103f86235d648d1eff89fa026bd14c13a3
Arnab Dutta <arnab.dutta558.ad@gmail.com>
2021-12-21 18:36:19 +0530

    Adding Binary Tree Codes

--- DS-ALGO/9. Binary Tree/binaryTree.py
+++ DS-ALGO/9. Binary Tree/binaryTree.py
@@ -1,3 +1,4 @@
+import queueLinkedList as queue
 class TreeNode:
     def __init__(self,data):
         self.data = data
@@ -5,12 +6,20 @@ class TreeNode:
         self.rightChild = None
     
 
-    
 newBTree = TreeNode("Drinks") #--Initialize Binary Tree by Creating Root Node
 leftChild = TreeNode("Hot") #-- Create Left Child
 rightChild = TreeNode("Cold") #-- Create Right Node
+tea = TreeNode("Tea")
+coffee = TreeNode("Coffee")
+cola = TreeNode("Cola")
+fanta = TreeNode("Fanta")
+leftChild.leftChild = tea
+leftChild.rightChild = coffee
+rightChild.leftChild = cola
+rightChild.rightChild = fanta
 newBTree.leftChild = leftChild #-- Add Right Child
 newBTree.rightChild = rightChild #-- Add Left Child
+
 
 def preOrderTraversal(rootNode):
     if not rootNode: # ----> O(1)
@@ -18,7 +27,39 @@ def preOrderTraversal(rootNode):
     print(rootNode.data)
     preOrderTraversal(rootNode.leftChild) # ----> O(n/2)
     preOrderTraversal(rootNode.rightChild) # ----> O(n/2)
+    
+    #---- Time Complexity --- O(n)
+    #---- Space Complexity --- O(n) [As we are using Stack Memory]
+
+def inOrderTraversal(rootNode):
+    if not rootNode: return #----> O(1)
+    inOrderTraversal(rootNode.leftChild) # ----> O(n/2)
+    print(rootNode.data)
+    inOrderTraversal(rootNode.rightChild) # ----> O(n/2)
 
     #---- Time Complexity --- O(n)
     #---- Space Complexity --- O(n) [As we are using Stack Memory]
+
+def postOrderTraversal(rootNode):
+    if not rootNode: return
+    postOrderTraversal(rootNode.leftChild)
+    postOrderTraversal(rootNode.rightChild)
+    print(rootNode.data)
+
+def levelOrderTraversal(rootNode):
+    if not rootNode: return
+    else:
+        customQueue = queue.Queue()
+        customQueue.enqueue(rootNode)
+        while not(customQueue.isEmpty()):
+            root = customQueue.dequeue()
+            print(root.value.data)
+
+
+
+print("Preorder Traversal : ")
 preOrderTraversal(newBTree)
+print("Inorder Traversal : ")
+inOrderTraversal(newBTree)
+print("PostOrder Traversal : ")
+postOrderTraversal(newBTree)
